f2a6ef3c09ef0a455bff90429e44a1524e5a58a0
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-12-21 23:55:27 +0900

    로그인시 jwt 토큰 발급

--- backend/src/router/loginRouter.js
+++ backend/src/router/loginRouter.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const {User} = require("../models");
 const {strConvertHash} = require("../util/utilLogic");
+const jwt = require("jsonwebtoken");
+const jwtSetting = require("../config/jwtSecret");
+const jwtConfig = require("../util/jwtConfig");
+const Result = require("../util/Result");
 
 const router = express.Router();
 
-router.post("/",async(req,res,next)=>{
-    try{
-        const {userId,password} = req.body;
+router.post("/", async (req, res, next) => {
+    try {
+        const {userId, password} = req.body;
         const hashPwd = strConvertHash(password);
-        console.log(`password:${password} hash:${hashPwd}`)
-        const users = await User.findOne({where:{userId,password:hashPwd}})
-        if(users===null){
-            res.json({message:"error"})
-        }else{
+        const users = await User.findOne({where: {userId: userId, password: hashPwd}});
+        if (users === null) {
+            res.json({message: "error"})
+        } else {
             const user2json = users.toJSON();
-            res.json(user2json);
+            const token = jwtConfig.jwtSign({
+                name: user2json.name,
+                id: user2json.id,
+                userId: user2json.userId,
+            });
+            if (token !== null) {
+                user2json['token'] = token
+                res.json(Result.successMsg(201, user2json));
+            }
+            else{
+                res.json(Result.failMsg(400,"jwt error",null))
+            }
         }
-    }catch (err){
+    } catch (err) {
+        console.log("err", err);
         next(err);
     }
 });
