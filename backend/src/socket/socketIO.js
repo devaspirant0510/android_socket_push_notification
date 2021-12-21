aa527c16faca5ebb4a47ffd98af8ac19c0134c2f
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-12-21 23:55:07 +0900

    namespace에서 room파서 채팅기능 구현, 퇴장기능

--- backend/src/socket/socketIO.js
+++ backend/src/socket/socketIO.js
@@ -2,12 +2,21 @@ const SocketIO = require("socket.io");
 
 module.exports = (server) => {
     const io = SocketIO(server, {path: "/socket.io"});
-
-    io.on("connection", function (socket) {
+    const room = io.of("chat");
+    room.on("connection", function (socket) {
         const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         console.log(`접속함 ${ip} 에서 `)
+        socket.join("room2");
+        console.log(socket.id);
+        console.log(room.to("room2"))
         socket.on("disconnect", function () {
+/*
+            socket.leave("room2",(a)=>{
+                console.log(a)
+            })
+*/
+            console.log(room.to("room2"))
             console.log(`연결 끊김 ${ip} 에서 `)
         })
         socket.emit("userCount",io.engine.clientsCount);
@@ -15,6 +24,11 @@ module.exports = (server) => {
             console.log(data);
             io.emit("message",data);
         });
+        socket.on("chat",data=>{
+            console.log("chat",data);
+            room.to("room2").emit("chat",data);
+
+        })
 
     });
 }
