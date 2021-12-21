a8a891d147aa019ded4f681f9710ad412e6cae8a
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-12-21 23:56:02 +0900

    jwt token 넘겨 방 생성

--- backend/src/router/roomRouter.js
+++ backend/src/router/roomRouter.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const {Room,User} = require("../models/")
+const {v4} = require("uuid");
+const jwt = require("jsonwebtoken");
+const {successMsg} = require("../util/Result");
 
 const router = express.Router();
 
-router.post("/",async(req,res,next)=>{
+router.post("/create",async(req,res,next)=>{
     try{
-
+        const {
+            token,
+            name
+        } = req.body;
+        const decode = jwt.verify(token,process.env.JWT_TOKEN);
+        console.log(decode.userId);
+        const result = await Room.create({
+            id:v4(),
+            name:name,
+            masterId:decode.id
+        });
+        const fullData =await Room.findOne({
+            where:{
+                masterId:decode.id
+            },
+            include:User
+        })
+        res.json(successMsg(201,fullData.toJSON()));
     }catch (err){
         next(err);
+    }
+});
+router.post("/join",async(req,res,next)=>{
+    try{
+
+    }catch(err){
+
     }
 });
 
