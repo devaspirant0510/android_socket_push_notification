2236f080b6ad35db77fc1f32781f7d3f872fd01b
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-01-12 18:11:03 +0900

    라우터 변경 에러처리 미들웨어 변경

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,17 +19,22 @@ app.set('view engine', 'html');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("port",process.env.PORT||8080);
-app.use(router);
+// api router
+app.use("/api",router);
 
 sequelize.sync({force:false}).then(value => {
     console.log("연결 성공");
 }).catch(reason => {
     console.log(reason);
 });
+
+// error handle middleware
 app.use((err,req,res,next)=>{
-    res.json(failMsg(200,"ERROR",err.toString()))
-})
+    res.json(failMsg(200,err))
+});
+
 const server = http.createServer(app);
+
 server.listen(app.get("port"),()=>{
     console.log("server is open");
 });
